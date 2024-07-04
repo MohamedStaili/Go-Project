e04008d4d059e3827c97e6cdb6d4885e4f18d492
MohamedStaili <5254mohamed@gmail.com>
2024-07-04 10:13:01 +0100

    Update

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"bytes"
 	"net/http"
+	"path/filepath"
 	"text/template"
 )
 
@@ -12,10 +14,37 @@ func Home(w http.ResponseWriter, r *http.Request) {
 func Contact(w http.ResponseWriter, r *http.Request) {
 	randerTemplate(w, "contact")
 }
-func randerTemplate(w http.ResponseWriter, tmpl string) {
-	t, arr := template.ParseFiles("./templates/" + tmpl + ".page.tmpl")
-	if arr != nil {
-		http.Error(w, arr.Error(), http.StatusInternalServerError)
+func randerTemplate(w http.ResponseWriter, tmplName string) {
+	templateCache, err := CreateTemplateCache()
+	if err != nil {
+		panic((err))
 	}
-	t.Execute(w, nil)
+	tmpl, ok := templateCache[tmplName+".page.tmpl"]
+	if !ok {
+		http.Error(w, "template not exist", http.StatusInternalServerError)
+		return
+	}
+	buffer := new(bytes.Buffer)
+	tmpl.Execute(buffer, nil)
+	buffer.WriteTo(w)
+}
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return cache, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		tmpl := template.Must(template.ParseFiles(page))
+		layouts, err := filepath.Glob("./templates/layouts/*.layout.tmpl")
+		if err != nil {
+			return cache, err
+		}
+		if len(layouts) > 0 {
+			tmpl.ParseGlob("./templates/layouts/*.layout.tmpl")
+		}
+		cache[name] = tmpl
+	}
+	return cache, nil
 }
